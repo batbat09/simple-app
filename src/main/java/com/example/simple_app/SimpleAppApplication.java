6173bfb6f092f6ccde03212c4f4d64192b12903b
babtbat09 <2220272@alfateh.upnm.edu.my>
2026-03-11 13:21:31 +0800

    Changed in JenkinsFile

--- src/main/java/com/example/simple_app/SimpleAppApplication.java
+++ src/main/java/com/example/simple_app/SimpleAppApplication.java
@@ -2,9 +2,18 @@ package com.example.simple_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimpleAppApplication {
+public class SimpleAppApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // This connects Spring to your external Tomcat
+        return application.sources(SimpleAppApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SimpleAppApplication.class, args);
     }
