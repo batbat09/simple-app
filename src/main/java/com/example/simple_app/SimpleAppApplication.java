2569920dc1caf24a85d067813e1e5cb566257858
babtbat09 <2220272@alfateh.upnm.edu.my>
2026-03-11 13:53:20 +0800

    Changed in JenkinsFile

--- src/main/java/com/example/simple_app/SimpleAppApplication.java
+++ src/main/java/com/example/simple_app/SimpleAppApplication.java
@@ -2,19 +2,27 @@ package com.example.simple_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.support.ErrorPageFilter;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SimpleAppApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        // This connects Spring to your external Tomcat
-        return application.sources(SimpleAppApplication.class);
-    }
+public class SimpleAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleAppApplication.class, args);
+    }
+
+    // Add this to prevent the "Possibly already registered?" error
+    @Bean
+    public ErrorPageFilter errorPageFilter() {
+        return new ErrorPageFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean<ErrorPageFilter> disableSpringBootErrorFilter(ErrorPageFilter filter) {
+        FilterRegistrationBean<ErrorPageFilter> filterRegistrationBean = new FilterRegistrationBean<>(filter);
+        filterRegistrationBean.setEnabled(false);
+        return filterRegistrationBean;
     }
 }
